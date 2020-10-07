637b7a56d62fadced4ec7a211058739c709831ff
Alex <alexandraagius@gmail.com>
2020-10-07 05:23:41 -0400

    3-->2?

--- celery_singleton/__init__.py
+++ celery_singleton/__init__.py
@@ -1,4 +1,4 @@
-from .singleton import Singleton, clear_locks
-from .exceptions import DuplicateTaskError
+from celery_singleton.singleton import Singleton, clear_locks
+from celery_singleton.exceptions import DuplicateTaskError
 
 __version__ = "0.2.0"
